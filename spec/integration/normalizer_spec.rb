2ebbde15cf8d898f0dce7069191d6da0bce2abe1
chrokh <christopher.okhravi@gmail.com>
2016-12-07 15:59:06 +0100

    Add integration test for when given hash without ids

--- spec/integration/normalizer_spec.rb
+++ spec/integration/normalizer_spec.rb
@@ -7,7 +7,23 @@ describe Normalizr, '.normalize!' do
   Schema = Normalizr::Schema
   ArrayOf = Normalizr::ArrayOf
 
-  context do
+  # Prepare schema
+  let(:comment) {
+    Schema.new('comments')
+  }
+  let(:author) {
+    Schema.new('authors')
+  }
+  let(:post) {
+    Schema.new('posts', {
+      author: author,
+      comments: ArrayOf.new(comment)
+    })
+  }
+
+
+  context 'with ids' do
+
     it 'normalizes' do
       obj = {
         posts: [
@@ -27,13 +43,6 @@ describe Normalizr, '.normalize!' do
           }
         ],
       }
-
-      comment = Schema.new('comments')
-      author = Schema.new('authors')
-      post = Schema.new('posts', {
-        author: author,
-        comments: ArrayOf.new(comment)
-      })
 
       actual = Normalizr.normalize!(obj, { posts: ArrayOf.new(post) })
       expected = {
@@ -60,6 +69,44 @@ describe Normalizr, '.normalize!' do
         }
       }
       expect(actual).to eq expected
+    end
+  end
+
+
+  context 'without ids' do
+    it 'normalizes and maintains references using generated ids' do
+      obj = {
+        posts: [
+          {
+            title: 'p0',
+            comments: [
+              { title: 'c00' },
+              { title: 'c01' },
+            ],
+            author: { name: 'a0' }
+          },
+          {
+            title: 'p1',
+            author: { name: 'a1' }
+          }
+        ],
+      }
+
+      actual = Normalizr.normalize!(obj, { posts: ArrayOf.new(post) })
+
+      # assert values
+      expect(actual[:authors].values[0][:name]).to eq 'a0'
+      expect(actual[:authors].values[1][:name]).to eq 'a1'
+      expect(actual[:posts].values[0][:title]).to eq 'p0'
+      expect(actual[:posts].values[1][:title]).to eq 'p1'
+      expect(actual[:comments].values[0][:title]).to eq 'c00'
+      expect(actual[:comments].values[1][:title]).to eq 'c01'
+
+      # assert relationships
+      expect(actual[:authors].keys[0]).to eq actual[:posts].values[0][:author]
+      expect(actual[:authors].keys[1]).to eq actual[:posts].values[1][:author]
+      expect(actual[:comments].keys[0]).to eq actual[:posts].values[0][:comments][0]
+      expect(actual[:comments].keys[1]).to eq actual[:posts].values[0][:comments][1]
     end
   end
 end
